2b306f28ffdbe1988e7ed6133343ec3ec4c0a333
digeesh038 <digeesh038@gmail.com>
2026-03-03 23:46:39 +0530

    commit

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -10,8 +10,8 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `${process.env.API_URL}/api/auth/google/callback`,
-            proxy: process.env.NODE_ENV === "production",
+            callbackURL: "/api/auth/google/callback",
+            proxy: true,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
